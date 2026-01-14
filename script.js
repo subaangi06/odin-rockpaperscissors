52d9aa4e2bc482d983876517a3432f0e88c2b8aa
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-14 17:14:35 +0530

    Initialise all functions required

---
+++ script.js
@@ -0,0 +1,74 @@
+function getComputerChoice(){
+    compChoice = Math.floor(Math.random()*3)+1;
+    // 1 - rock, 2-paper, 3-scissors
+    switch (compChoice) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+
+    }
+
+}
+
+choiceTest = getComputerChoice();
+console.log(choiceTest);
+
+//function to prompt user for their choice
+function getHumanChoice(){
+    choice = prompt("Enter your choice, rock, paper or scissors?").toLowerCase();
+    //REMOVE AFTER YOURE DONE PLEASE TY (TESTING)
+    console.log(choice);
+    return choice;
+}
+
+//initializing score variables
+var humanScore=0;
+var computerScore=0;
+
+//syntax for a single round of the game
+function playRound(humanChoice,computerChoice){
+    if (computerChoice==="rock"){
+        if (humanChoice==="rock"){
+            //RR
+            console.log("It's a tie!");
+        } else if (humanChoice==="paper"){
+            //RP
+            console.log("You win! Paper beats rock.");
+        } else {
+            //RS
+            console.log("You lose! Rock beats scissors.")
+        }
+    } else if (computerChoice==="paper"){
+        if (humanChoice ==="rock"){
+            //PR
+            console.log("You lose! Paper beats rock.");
+        } else if (humanChoice==="paper"){
+            //PP
+            console.log("It's a tie!");
+        } else{
+            //PS
+            console.log("You win! Scissors beat paper.");
+        }
+    } else{
+        if (humanChoice ==="rock"){
+            //SR
+            console.log("You win! Rock beats scissors.");
+        } else if (humanChoice ==='paper'){
+            //SP
+            console.log("You lose! Scissors beat paper.");
+        } else{
+            //SS
+            console.log("It's a tie!");
+        }
+    }
+
+}
+
+
+//test function
+const humanSelection = getHumanChoice();
+const computerSelection = choiceTest;
+playRound(humanSelection, computerSelection);
