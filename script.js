f20efe4231f51a9fe5a627148f360afc2b7544cd
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-20 12:12:40 +0530

    Add buttons, change round logic, change console log to display in page itself (DOM)

--- script.js
+++ script.js
@@ -13,86 +13,70 @@ function getComputerChoice(){
 
 }
 
-//function to prompt user for their choice
-function getHumanChoice(){
-    choice = prompt("Enter your choice, rock, paper or scissors?").toLowerCase();
-    return choice;
-}
+let humanScore=0;
+let computerScore=0;
 
+
+const body = document.querySelector("body");
+const outputDiv = document.createElement("div");
+outputDiv.textContent = "Results will show here as each round is played!"
+body.appendChild(outputDiv)
 
 function playGame(){
-    //initializing score variables
-    let humanScore=0;
-    let computerScore=0;    
-
-    //syntax for a single round of the game
+    
     function playRound(humanChoice,computerChoice){
         if (computerChoice==="rock"){
             if (humanChoice==="rock"){
                 //RR
-                console.log("It's a tie!");
+                outputDiv.textContent("It's a tie!");
             } else if (humanChoice==="paper"){
                 //RP
-                console.log("You win! Paper beats rock.");
+                outputDiv.textContent("You win! Paper beats rock.");
                 humanScore++;
             } else {
                 //RS
-                console.log("You lose! Rock beats scissors.")
+                outputDiv.textContent("You lose! Rock beats scissors.")
                 computerScore++;
             }
         } else if (computerChoice==="paper"){
             if (humanChoice ==="rock"){
                 //PR
-                console.log("You lose! Paper beats rock.");
+                outputDiv.textContent("You lose! Paper beats rock.");
                 computerScore++;
             } else if (humanChoice==="paper"){
                 //PP
-                console.log("It's a tie!");
+                outputDiv.textContent("It's a tie!");
             } else{
                 //PS
-                console.log("You win! Scissors beat paper.");
+                outputDiv.textContent("You win! Scissors beat paper.");
                 humanScore++;
             }
         } else{
             if (humanChoice ==="rock"){
                 //SR
-                console.log("You win! Rock beats scissors.");
+                outputDiv.TextContent("You win! Rock beats scissors.");
                 humanScore++;
             } else if (humanChoice ==='paper'){
                 //SP
-                console.log("You lose! Scissors beat paper.");
+                outputDiv.TextContent("You lose! Scissors beat paper.");
                 computerScore++;
             } else{
                 //SS
-                console.log("It's a tie!");
+                outputDiv.TextContent("It's a tie!");
             }
         }
 
     }
-
-    //looping 5 times for 5 separate rounds of rock paper scissors.     
-    for(let i=1; i<=5; i++){
-        //storing choices in respective variables
-        console.log(`Round ${i}`)
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();    
-        console.log(`You chose ${humanSelection} and the computer chose ${computerSelection}`);
-        playRound(humanSelection, computerSelection);
-    }
-
-    //function to calculate the user with higer score. 
-    function calcWinner(cScore, hScore){
-        if (cScore > hScore){
-            console.log("The computer scored higher than you :(. The computer wins!!");
-        } else if (hScore>cScore){
-            console.log("You scored higher than the computer :) You win!!");
-        } else{
-            console.log("You both scored an equal number of points, you both win!!");
-        }
-    }
-
-    calcWinner(computerScore, humanScore);
-
 }
 
-playGame();
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", ()=> {
+        const computerChoice = getComputerChoice();
+        const humanChoice = button.id;
+
+        playRound(humanChoice, computerChoice);
+    })
+})
+
+
